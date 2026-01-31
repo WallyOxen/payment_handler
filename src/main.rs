9b01602015e3565931a34da1ef6ced53c7e735dc
WallyOxen <wally@wallys.world>
2026-01-31 15:49:49 -0500

    Basic stdin csv parsing based on csv crates docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+
+use csv::{ReaderBuilder, Trim};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Transaction {
+    #[serde(rename = "type")]
+    transaction_type: TransactionType,
+    client: u16,
+    #[serde(rename = "tx")]
+    transaction_id: u32,
+    amount: Option<f64>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum TransactionType {
+    Chargeback,
+    Deposit,
+    Dispute,
+    Resolve,
+    Withdrawl,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut reader = ReaderBuilder::new()
+        .trim(Trim::All)
+        .from_reader(io::stdin());
+
+    for result in reader.deserialize() {
+        let transaction: Transaction = result?;
+        println!("{:#?}", transaction);
+    }
+    Ok(())
 }
